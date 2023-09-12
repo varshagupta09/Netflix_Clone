d2abf7f13cedd143f03d27d2d334741829bfc0fc
Varsha Gupta <vrsa8896@gmail.com>
2023-09-12 22:41:31 +0530

    Netflix

---
+++ script.js
@@ -0,0 +1,15 @@
+// var q = document.getElementsByClassName("que");
+// var i;
+
+// for(i=0; i<q.length; i++){
+//     q[i].addEventListener("click",function(){
+//         this.classList.toggle(active);
+
+//         var ans = this.nextElementSibling;
+//         if(ans.style.display === "block") {
+//             ans.style.display = "none";
+//         } else {
+//             ans.style.display = "block";
+//         }
+//     });
+// }
